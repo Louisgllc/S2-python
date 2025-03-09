06e16cc12d53a5ea9ba68335280d2d60f0b1382a
Louisgllc <louis.gaillac@gmail.com>
2025-03-09 23:26:12 +0100

    maj

--- TD/TD2/début programme 2.py
+++ TD/TD2/début programme 2.py
@@ -1,38 +1,45 @@
 import tkinter as tk 
 import random as rd
 
-
-
 window = tk.Tk()
 window.title("interface exo 2")
-canva = tk.Canvas(window, width=800, height=600, bg="black")
+canva = tk.Canvas(window, width=720, height=480, bg="black", bd=7,
+                          relief="raised", highlightthickness=10)
 
-wi=800
-he=600
-
+wi=720
+he=480
+#choisir la couleur
 def choix_couleur():
-    tk.Entry("couleur")
+    color= tk.Entry(window, width=25)
+    color.pack()
+    print("couleur changé !")
+
 def cercle(): 
-    x=rd.randint(50, wi -50)
-    y=rd.randint(50, he -50)
-    canva.create_oval(x-50, y-50, x+50, y+50, fill=color)
+    x=rd.randint(67, wi -67)
+    y=rd.randint(67, he -67)
+    canva.create_oval(x-50, y-50, x+50, y+50, fill="blue")
     
 
 def carre():
-    canva.create_rectangle()
+    L=rd.randint(100, wi -100)
+    l=rd.randint(100, he -100)
+    canva.create_rectangle(L, l, L+100, l+100, fill="red")
 
 def croix():
-    canva.create_oval()
+    z=rd.randint(100, wi-100)
+    q=rd.randint(100, he-100)
+    canva.create_line(z, q, z+100,  fill="yellow")
 
 
-b1 = tk.Button(window, text="choisir la couleur", padx=50, pady=10, 
-                       bg="lightgrey")
-b2 = tk.Button(window, text="cercle", padx=25, pady=5, 
-                       bg="lightgrey", command=cercle)
-b3 = tk.Button(window, text="carré", padx=25, pady=5,
-                       bg="lightgrey", command=carre)
-b4 = tk.Button(window, text="croix", padx=25, pady=5, 
-                       bg="lightgrey", command=croix)
+
+b1 = tk.Button(window, text="choisir la couleur", font=("Georgia"), padx=50, pady=10, 
+                       bg="lightgrey", bd=3, relief="ridge", command=choix_couleur)
+b2 = tk.Button(window, text="cercle", font=("Georgia"), padx=25, pady=5, 
+                       bg="lightgrey", bd=3, relief="raised", command=cercle)
+b3 = tk.Button(window, text="carré", font=("Georgia"), padx=25, pady=5,
+                       bg="lightgrey", bd=3, relief="raised", command=carre)
+b4 = tk.Button(window, text="croix", font=("Georgia"), padx=25, pady=5, 
+                       bg="lightgrey",bd=3, relief="raised", command=croix)
 
 b1=b1.grid(column=1, row=0)
 b2=b2.grid(column=0, row=1)
@@ -40,4 +47,6 @@ b3=b3.grid(column=0, row=2)
 b4=b4.grid(column=0, row=3)
 
 canva.grid(row=1, column=1, rowspan=3)
+
+#pour afficher la fenetre
 window.mainloop()
